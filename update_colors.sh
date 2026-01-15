6b2b9aa1e39b9a6b951fb1a6aa479c71879b04be
llawn <llawn06@gmail.com>
2026-01-15 19:54:59 +0100

    Fix JS array syntax and update script

--- update_colors.sh
+++ update_colors.sh
@@ -5,28 +5,30 @@ lua_file="lua/llawn/colors/colors.lua"
 js_file="lua/llawn/colors/palette_generator.js"
 
 # Extract the colors array from Lua and convert to JS
-sed -n '/local colors = {/,/}/p' "$lua_file" | \
-sed 's/local colors = {/const colorDatabase = [/; s/}$/];/' | \
-sed 's/0x/0x/g' | \
-sed 's/"\([^"]*\)"/"\1"/g' > temp_colors.js
+sed -n '/^local colors = {/,/^}/p' "$lua_file" | \
+sed '1s/local colors = {/const colorDatabase = [/; $s/^  }$/];/' > temp_colors.js
 
-# Replace the old array in the JS file
-# Find the line with // Colors from your colors.lua database
-# And replace until the ]; 
-
-# Use sed to replace the block
-start_marker="// Colors from your colors.lua database"
-end_marker="];"
-
-# Get the content before start
-head -n "$(grep -n "$start_marker" "$js_file" | cut -d: -f1)" "$js_file" > temp_js
-
-# Add the new array
-echo "$start_marker" >> temp_js
-cat temp_colors.js >> temp_js
-
-# Add the rest after ];
-tail -n +$(($(grep -n "$end_marker" "$js_file" | cut -d: -f1) + 1)) "$js_file" >> temp_js
+# Replace the array in the JS file using awk
+awk -v colors_file="temp_colors.js" '
+BEGIN {
+    in_array = 0
+    printed_colors = 0
+}
+{
+    if ($0 ~ /\/\/ Colors from your colors.lua database/ && !printed_colors) {
+        print $0
+        while ((getline line < colors_file) > 0) {
+            print line
+        }
+        printed_colors = 1
+        in_array = 1
+    } else if (in_array && ($0 ~ /\];/ || $0 ~ /^  }/)) {
+        in_array = 0
+    } else if (!in_array) {
+        print $0
+    }
+}
+' "$js_file" > temp_js
 
 # Replace the file
 mv temp_js "$js_file"
